fdcaa2d7f9fe779193fd17ece938168b84c99ef9
pisarukka-hub <pisaruk.k.a@gmail.com>
2026-08-02 23:21:33 +0400

    modified:   Basic structures/.DS_Store 	modified:   Basic structures/Data input and output.Operations with numbers and strings. Formatting/formatting03.py

--- Basic structures/Data input and output.Operations with numbers and strings. Formatting/formatting03.py
+++ Basic structures/Data input and output.Operations with numbers and strings. Formatting/formatting03.py
@@ -7,9 +7,9 @@ coststr = str(weight) + "kg * " + str(cost) + "USD/kg"
 total = cost * weight
 change = money - total
 print(f"{bill:=^35}")
-print(f"product:{name:>29}")
-print(f"Price:{coststr:>30}")
-print(f"Total:{total:>26}USD")
-print(f"Paid:{money:>24}USD")
-print(f"Change:{change:>26}USD")
+print(f"Product:{name: >27}")
+print(f"Price:{coststr: >29}")
+print(f"Total:{total: >26}USD")
+print(f"Paid:{money: >27}USD")
+print(f"Change:{change: >25}USD")
 print("=" * 35)
