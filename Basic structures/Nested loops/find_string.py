4418ceef6a3beabc6f38637512680458733f5ca9
pisarukka-hub <pisaruk.k.a@gmail.com>
2026-09-14 18:38:43 +0400

    modified:   Basic structures/Nested loops/find_string.py

--- Basic structures/Nested loops/find_string.py
+++ Basic structures/Nested loops/find_string.py
@@ -1,6 +1,11 @@
+# How many blocks of strings contain the word "rabbit"?
+flag = False
 answer = 0
-for i in range(int(input())):
-    while ((k := str(input())) != "ВСЁ"):
-        if (k == "зайка"):
-            answer += 1
+for i in range(n := int(input())):
+    while ((k := str(input())) != "All"):
+        if (k == "rabbit"):
+            flag = True
+    if (flag):
+        answer += 1
+    flag = False
 print(answer)
