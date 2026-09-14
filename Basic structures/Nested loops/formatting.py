defe7ce6d63c7b30a56296547a10d42fc37e0cdf
pisarukka-hub <pisaruk.k.a@gmail.com>
2026-09-14 15:46:08 +0400

    modified:   Basic structures/Nested loops/formatting.py

--- Basic structures/Nested loops/formatting.py
+++ Basic structures/Nested loops/formatting.py
@@ -1,10 +1,14 @@
+# Formatting numbers in a triangular pattern
+linesize = 0
 line = ""
-k = 1
+endline = 1
 N = int(input())
 for i in range(1, N + 1):
-    if (k <= N ):
-        line = line + str(N) + " "
+    if (i <= endline):
+        line = line + str(i) + " "
     else:
         print(line[:-1])
-        line = ""
-        k = k + N + 1
+        line = str(i) + " "
+        linesize += 1
+        endline = i + linesize
+print(line[:-1])
